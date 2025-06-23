3c3ef9ca7b284597f99b1f6904dbdc33d52b0c9b
Kyle Johnston <flow14@gmail.com>
2025-06-23 18:59:47 -0500

    update title

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = 'Kyle IO';
+export const SITE_TITLE = 'Kyle IO, a creative journal';
 export const SITE_DESCRIPTION = 'A collection of things I\'ve made, written, and that spark my curiosity';
