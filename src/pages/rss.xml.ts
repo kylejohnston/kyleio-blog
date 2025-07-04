4967629b45b75f8e54d61554e5af937be00ddd21
Kyle Johnston <flow14@gmail.com>
2025-07-04 15:31:15 -0500

    clean up commented code

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -64,26 +64,3 @@ export async function GET(context: APIContext) {
     items: feedItems,
   });
 }
-
-// import rss from '@astrojs/rss';
-// import { getCollection } from 'astro:content';
-// import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
-
-// export async function GET(context) {
-//   const blog = await getCollection('posts', ({ data }) => {
-//     return !data.draft;
-//   });
-//   return rss({
-//     title: SITE_TITLE,
-//     description: SITE_DESCRIPTION,
-//     site: context.site,
-//     items: blog.map((post) => ({
-//       title: post.data.title,
-//       pubDate: post.data.pubDate,
-//       description: post.data.description,
-//       link: `/p/${post.slug}/`,
-//     })),
-//   });
-// }
-
-
