deb2ab379d1f30677ec9e1a5abc1f305f4f2128a
Kyle Johnston <flow14@gmail.com>
2025-07-04 14:13:54 -0500

    Add site_url

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,3 @@
+export const SITE_URL = 'https://kyle.io';
 export const SITE_TITLE = 'Kyle IO, a creative journal';
 export const SITE_DESCRIPTION = 'A collection of things I\'ve made, written, and that spark my curiosity';
