7d3838eec0479cb60dedbe364cc0682ef6d627a7
Kyle Johnston <flow14@gmail.com>
2025-07-04 15:02:41 -0500

    Add a comment for future me

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,6 +2,7 @@ import { defineCollection, z } from 'astro:content';
 
 const posts = defineCollection({
   type: 'content',
+	// Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
     description: z.string(),
