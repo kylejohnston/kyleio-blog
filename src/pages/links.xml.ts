1bfc89b334430706ffa84838c080e59704b668b3
Kyle Johnston <flow14@gmail.com>
2025-08-03 20:41:44 -0500

    Initial commit

---
+++ src/pages/links.xml.ts
@@ -0,0 +1,25 @@
+import rss from '@astrojs/rss';
+import type { APIContext } from 'astro';
+import { SITE_DESCRIPTION, SITE_TITLE, SITE_URL } from '../consts';
+
+export async function GET(context: APIContext) {
+  const response = await fetch(`https://api.raindrop.io/rest/v1/raindrops/58403513`, {
+    headers: {
+      "Authorization": `Bearer ${import.meta.env.RAINDROP_TOKEN}`
+    }
+  });
+  const data = await response.json();
+  const links = data.items;
+
+  return rss({
+    title: `${SITE_TITLE} - Links`,
+    description: SITE_DESCRIPTION,
+    site: `${SITE_URL}/links`,
+    items: links.map((link: any) => ({
+      title: link.title,
+      pubDate: link.created,
+      description: link.excerpt,
+      link: link.link,
+    })),
+  });
+}
