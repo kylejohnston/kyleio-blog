4755f737f66095f0172fe79c857708a60eaa0cba
Kyle Johnston <flow14@gmail.com>
2025-08-04 20:16:52 -0500

    Update site_title

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Kyle IO, a creative journal';
+export const SITE_TITLE = 'Kyle Johnston\'s creative journal';
 export const SITE_DESCRIPTION = 'A collection of things I\'ve made, written, and that spark my curiosity';
 export const SITE_URL = 'https://kyle.io';
